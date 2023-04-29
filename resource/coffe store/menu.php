18a62cc1ebde09b557a060a1e90ebfc3bd4142c2
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-04-29 18:34:47 +0200

    in page of menu display added real product and category

--- resource/coffe store/menu.php
+++ resource/coffe store/menu.php
@@ -452,150 +452,44 @@
                         <div class="popular-menu__wrapper m-auto d-inline-block">
                             <div class="popular-menu__filter d-flex justify-content-center align-items-center mb-45">
                                 <button class="active" data-filter="*">All</button>
-                                <button data-filter=".cat1">chocolate</button>
-                                <button data-filter=".cat2">coffee</button>
-                                <button data-filter=".cat3">sandwiches</button>
-                                <button data-filter=".cat4">sweets</button>
-                                <button data-filter=".cat5">Black tea</button>
-                                <button data-filter=".cat6">Grean Tea</button>
+                                <?php
+                                    include "../Admin/include/connection.php";
+                                    $sql = "SELECT id, name FROM category"; // get categories from the database
+                                    $result = $db->query($sql);
+                                    if ($result->rowCount() > 0) {
+                                        foreach ($result as $row) {
+                                            echo '<button data-filter=".cat'.$row['id'].'">'.$row['name'].'</button>';
+                                        }
+                                    }
+                                ?>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div id="popular-menu-grid-2" class="row popular-menu__grid popular-menu__lists mt-none-30">
                     <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-sizer"></div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat2 cat3 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-1.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">Americano Rosted Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat3 cat4 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-2.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">JAPANIS COFFEE GRED</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat2 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-3.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">CAFÈ AU LAIT Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat4 cat3 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-4.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">ESPRESSO RISTRETTO Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat2 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-5.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">VIRGIN COFFEE GRED</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-6.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">ESPRESSO RISTRETTO Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat2 cat3 cat4 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-1.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">BRAZIL COFFEE GRED</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-2.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">ESPRESSO RISTRETTO BEANS</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat2 cat3 cat4 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-3.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">CAFFÈ LATTE BEANS</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-4.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">Turkish Rosted Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat2 cat3 cat4 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-5.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">Bangladeshi Rosted Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat5 cat6 mt-35">
-                        <div class="popular-menu__list">
-                            <div class="thumb">
-                                <img src="assets/images/menu/menu-2-6.png" alt="">
-                            </div>
-                            <div class="content">
-                                <h3 class="title border-effect"><a href="#0">indian Rosted Coffee</a></h3>
-                                <span class="price">$12.00</span>
-                            </div>
-                        </div>
-                    </div>
+                    <?php
+                        include "../Admin/include/connection.php";
+                        $sql = "SELECT image, name, price FROM product";
+                        $result = $db->query($sql);
+                        if ($result->rowCount() > 0) {
+                            foreach ($result as $row) {
+                    ?>
+                                <div class="col-xl-6 col-lg-6 col-md-6 l-item grid-item cat1 cat2 cat3 mt-35">
+                                    <div class="popular-menu__list">
+                                        <div class="thumb">
+                                            <img src="../Admin/uploads/product/<?php echo $row['image']; ?>" alt="">
+                                        </div>
+                                        <div class="content">
+                                            <h3 class="title border-effect"><a href="#0"><?php echo $row['name']; ?></a></h3>
+                                            <span class="price">$<?php echo $row['price']; ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+                    <?php
+                            }
+                        }
+                    ?>
                 </div>
                 <div class="row">
                     <div class="col-xl-12 text-center">
